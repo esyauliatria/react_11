f9f726fdf5c8ffbbb1a88bf8533e4f383dcca2b3
esyauliatria <esyaulia300@gmail.com>
2025-10-29 10:42:39 +0700

    Update Modul 2

---
+++ src/pages/Produk.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Home(){
+    return(
+        <div>
+            <h1>Selamat Datang di Halaman Produk</h1>
+        </div>
+    );
+};
+
+export default Produk;
